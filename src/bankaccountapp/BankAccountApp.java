a60ae94468d605cca5d8d10b60ebd715f1a40652
Dilan Loyola <ldilan6@gmail.com>
2020-12-29 21:38:46 -0500

    Read new accounts from CSV file, create a list of Objects.

--- src/bankaccountapp/BankAccountApp.java
+++ src/bankaccountapp/BankAccountApp.java
@@ -1,9 +1,17 @@
 package bankaccountapp;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class BankAccountApp {
 
 	public static void main(String[] args) {
-		Checking chkacc1 = new Checking("Tom Wilson", "321456789", 1500);
+		List<Account> accounts = new LinkedList<Account>();
+		
+		
+		String file = "C:\\Users\\ldila\\Downloads\\NewBankAccounts.csv";
+		
+		/*Checking chkacc1 = new Checking("Tom Wilson", "321456789", 1500);
 		
 		Savings savacc1 = new Savings("Rich Lowe", "456789123", 2500);
 		
@@ -13,12 +21,36 @@ public class BankAccountApp {
 		
 		System.out.println("**********************************");
 		
-		chkacc1.showInfo();
+		chkacc1.showInfo();*/
 		
 		
 		
 		// Read a CSV file then create new accounts based on that data
+		
+		List<String[]> newAccountHolders = utilities.CSV.read(file);
+		for(String[] accountHolder : newAccountHolders) {
+			String name = accountHolder[0];
+			String social = accountHolder[1];
+			String accountType = accountHolder[2];
+			double initDeposit = Double.parseDouble(accountHolder[3]);
+			if(accountType.equals("Savings")) {
+				accounts.add(new Savings(name, social, initDeposit));
+			}
+			else if (accountType.equals("Checking")){
+				accounts.add(new Checking(name, social, initDeposit));
+			}
+			else {
+				System.out.println("ERROR READING ACCOUNT TYPE");
+			}
+		}
+		
 
+		
+		for(Account acc : accounts) {
+			System.out.println("\n*********************");
+			acc.showInfo();
+			
+		}
 	}
 
 }
